b44b4539c8c6e0669e22e415106f3d8e49050fcb
Sidharth <sidmailpersonal@gmail.com>
2017-09-21 23:50:56 +0530

    added project page

--- server.js
+++ server.js
@@ -45,6 +45,12 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects Page'
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Server is running on ${port}`);
 });
